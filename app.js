8bc8e1fa79e830a9c6d9941eeb2daa7d32accc6b
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-03-09 17:54:22 -0400

    views finished

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var morgan = require('morgan');
+var swig = require('swig');
 
 var app = express();
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.engine('html', swig.renderFile);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
 app.get('/', function (req,res) {
-	res.send('hello, world!')
-})
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
+
 
 var server = app.listen(3000, function() {
 	var host = server.address().address
 	var port = server.address().port
-
 console.log('Listening at http://%s:%s', host, port)
 
 })
